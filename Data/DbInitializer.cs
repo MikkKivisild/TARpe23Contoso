67d8e6165ad304b862aecd3473c70e42e15592fa
opilanegsgasg <opilane@HP-CZC321705Zgsgsdg>
2024-08-26 17:34:39 +0300

    a

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,66 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.Identity.Client;
+
+namespace ContosoUniversity.Data
+{
+    public class DbInitializer
+    {
+        public static void Initializer(SchoolContext context)
+        {
+            context.Database.EnsureCreated();
+            if(context.Students.Any())
+            {
+                return;
+            }
+            var students = new Student[]
+            {
+                new Student{FirstName="Artkom", LastName="Skatškov", EnrollmentDate=DateTime.Parse
+                ("2069-04-20")}
+            };
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8131be7c9235bdc4711a0e1a4d7a2a78d6fbb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
