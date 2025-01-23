83527d35d70fced98c73975bfe9c86630674984e
mikkm <mikkm@DESKTOP-6OR357K>
2025-01-23 16:28:35 +0200

    Instructors Delete

--- Controllers/InstructorsController.cs
+++ Controllers/InstructorsController.cs
@@ -123,6 +123,29 @@ namespace ContosoUniversity.Controllers
         }
 
 
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var instructor = await _context.Instructors
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (instructor == null)
+            {
+                return NotFound();
+            }
+            return View(instructor);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var instructor = await _context.Instructors.FindAsync(id);
+            _context.Instructors.Remove(instructor);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
 
         private void PopulateAssignedCourseData(Instructor instructor)
