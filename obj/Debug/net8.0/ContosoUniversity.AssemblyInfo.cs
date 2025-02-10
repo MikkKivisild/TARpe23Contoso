13673ec30ec5b84379f766a994ed15aeb39c10c2
mikkm <mikkm@DESKTOP-6OR357K>
2025-02-10 06:42:00 +0200

    removal for commits

--- obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoUniversity.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65019f8ec4e10d22452113b647581165f0dd065e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06f6157df5321c3078a9d52b5058b32ac788d781")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoUniversity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
