b6fd9a5dd1bd2a84e6514923724fb6611d136e8c
mikkm <mikkm@DESKTOP-6OR357K>
2025-02-10 06:43:51 +0200

    Departments - Delete

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -60,9 +60,31 @@ namespace ContosoUniversity.Controllers
             return View(department);
 
         }
-       
-        
 
-       
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var department = await _context.Departments
+                .FirstOrDefaultAsync(m => m.DepartmentID == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return View(department);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var department = await _context.Departments.FindAsync(id);
+            _context.Departments.Remove(department);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
